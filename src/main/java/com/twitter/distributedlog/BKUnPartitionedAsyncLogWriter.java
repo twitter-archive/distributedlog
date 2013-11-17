b2f3d0e9ec3509328c4eafca613b7bf8af245c7e
Robin Dhamankar <rdhamankar@twitter.com>
2013-11-17 08:21:31 -0800

    Async StartLogSegment

--- src/main/java/com/twitter/distributedlog/BKUnPartitionedAsyncLogWriter.java
+++ src/main/java/com/twitter/distributedlog/BKUnPartitionedAsyncLogWriter.java
@@ -1,13 +1,28 @@
 package com.twitter.distributedlog;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import com.twitter.util.ExecutorServiceFuturePool;
+import com.twitter.util.ExceptionalFunction;
+import com.twitter.util.ExceptionalFunction0;
 import com.twitter.util.Future;
+
 import java.io.IOException;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class BKUnPartitionedAsyncLogWriter extends BKUnPartitionedLogWriterBase implements AsyncLogWriter {
 
+    private final ExecutorService executorService;
+    private final ExecutorServiceFuturePool futurePool;
+
+
     public BKUnPartitionedAsyncLogWriter(DistributedLogConfiguration conf, BKDistributedLogManager bkdlm) throws IOException {
         super(conf, bkdlm);
+        executorService = Executors.newScheduledThreadPool(1,
+            new ThreadFactoryBuilder().setNameFormat("BKALW-" + bkdlm.name + "-executor-%d").build());
+        futurePool = new ExecutorServiceFuturePool(executorService);
     }
 
     /**
@@ -16,12 +31,34 @@ public class BKUnPartitionedAsyncLogWriter extends BKUnPartitionedLogWriterBase 
      * @param record single log record
      */
     @Override
-    public Future<DLSN> write(LogRecord record) throws IOException {
+    public Future<DLSN> write(final LogRecord record) throws IOException {
         if ((record.getTransactionId() < 0) ||
             (record.getTransactionId() == DistributedLogConstants.MAX_TXID)) {
             throw new IOException("Invalid Transaction Id");
         }
 
-        return getLedgerWriter(DistributedLogConstants.DEFAULT_STREAM, record.getTransactionId()).write(record);
+        return futurePool.apply(new ExceptionalFunction0<BKPerStreamLogWriter>() {
+            public BKPerStreamLogWriter applyE() throws IOException {
+                return getLedgerWriter(DistributedLogConstants.DEFAULT_STREAM, record.getTransactionId());
+            }
+        }).flatMap(new ExceptionalFunction<BKPerStreamLogWriter, Future<DLSN>>() {
+            public Future<DLSN> applyE(BKPerStreamLogWriter w) throws IOException {
+                return w.write(record);
+            }
+        });
+    }
+
+    private void closeAndCompleteSync() throws IOException {
+        super.closeAndComplete();
+    }
+
+    @Override
+    public void closeAndComplete() throws IOException {
+        futurePool.apply(new ExceptionalFunction0<Integer>() {
+            public Integer applyE() throws IOException {
+                closeAndCompleteSync();
+                return 0;
+            }
+        }).get();
     }
 }
