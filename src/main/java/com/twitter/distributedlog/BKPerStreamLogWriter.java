2a97d71696af124e5d3f007eb627da3f3e19784e
Robin Dhamankar <rdhamankar@twitter.com>
2013-11-21 13:24:02 -0800

    Periodic Flush change

--- src/main/java/com/twitter/distributedlog/BKPerStreamLogWriter.java
+++ src/main/java/com/twitter/distributedlog/BKPerStreamLogWriter.java
@@ -579,26 +579,23 @@ class BKPerStreamLogWriter implements PerStreamLogWriter, AddCallback, Runnable 
                     + BKException.getMessage(rc) + "\""
                     + " but is already (" + transmitResult.get() + ") \""
                     + BKException.getMessage(transmitResult.get()) + "\"");
+                transmitPacketSize.registerFailedEvent(transmitPacket.buffer.getLength());
+            } else {
+                // If we had data that we flushed then we need it to make sure that
+                // background flush in the next pass will make the previous writes
+                // visible by advancing the lastAck
+                periodicFlushNeeded |= !transmitPacket.isControl();
+                transmitPacketSize.registerSuccessfulEvent(transmitPacket.buffer.getLength());
             }
         }
 
         transmitPacket.processTransmitComplete(entryId, transmitResult.get());
 
         synchronized (this) {
-            if (BKException.Code.OK == rc) {
-                if (!transmitPacket.isControl()) {
-                    if (lastDLSN.compareTo(transmitPacket.getLastDLSN()) < 0) {
-                        lastDLSN = transmitPacket.getLastDLSN();
-                    }
-
-                    // If we had data that we flushed then we need it to make sure that
-                    // background flush in the next pass will make the previous writes
-                    // visible by advancing the lastAck
-                    periodicFlushNeeded = true;
+            if (BKException.Code.OK == rc && !transmitPacket.isControl()) {
+                if (lastDLSN.compareTo(transmitPacket.getLastDLSN()) < 0) {
+                    lastDLSN = transmitPacket.getLastDLSN();
                 }
-                transmitPacketSize.registerSuccessfulEvent(transmitPacket.buffer.getLength());
-            } else {
-                transmitPacketSize.registerFailedEvent(transmitPacket.buffer.getLength());
             }
             releasePacket(transmitPacket);
             CountDownLatch l = syncLatch;
