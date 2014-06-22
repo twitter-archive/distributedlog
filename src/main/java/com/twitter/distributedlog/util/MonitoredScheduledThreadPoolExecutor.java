afe6a3ca62423befb3ca232d5284abba572ee22b
Robin Dhamankar <rdhamankar@twitter.com>
2014-06-22 12:01:17 -0700

    DistributedLog: Log unhandled exceptions - Add methods to log or handle uncaught exceptions for thread pool executors use by distributed log - If the application has set the unhanded exception handler; pass the exceptions in task execution to the handler - Log all such exceptions so we have a trace of any such unexpected exception
    RB_ID=390711

--- src/main/java/com/twitter/distributedlog/util/MonitoredScheduledThreadPoolExecutor.java
+++ src/main/java/com/twitter/distributedlog/util/MonitoredScheduledThreadPoolExecutor.java
@@ -4,14 +4,19 @@ import org.apache.bookkeeper.stats.Gauge;
 import org.apache.bookkeeper.stats.OpStatsLogger;
 import org.apache.bookkeeper.stats.StatsLogger;
 import org.apache.bookkeeper.util.MathUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 public class MonitoredScheduledThreadPoolExecutor extends ScheduledThreadPoolExecutor {
+    static final Logger LOG = LoggerFactory.getLogger(MonitoredScheduledThreadPoolExecutor.class);
 
     private class TimedRunnable implements Runnable {
 
@@ -136,4 +141,62 @@ public class MonitoredScheduledThreadPoolExecutor extends ScheduledThreadPoolExe
     public <T> Future<T> submit(Callable<T> task) {
         return super.submit(timedCallable(task));
     }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        super.afterExecute(r, t);
+        Throwable hiddenThrowable = extractThrowable(r);
+        if (hiddenThrowable != null)
+            logAndHandle(hiddenThrowable, true);
+
+        // The executor re-throws exceptions thrown by the task to the uncaught exception handler
+        // so we don't need to pass the exception to the handler explicitly
+        if (null != t) {
+            logAndHandle(t, false);
+        }
+    }
+
+    /**
+     * The executor re-throws exceptions thrown by the task to the uncaught exception handler
+     * so we only need to do anything if uncaught exception handler has not been se
+     */
+    private void logAndHandle(Throwable t, boolean passToHandler) {
+        if (Thread.getDefaultUncaughtExceptionHandler() == null) {
+            LOG.error("Unhandled exception on thread {}", Thread.currentThread().getName(), t);
+        }
+        else {
+            LOG.info("Unhandled exception on thread {}", Thread.currentThread().getName(), t);
+            if (passToHandler) {
+                Thread.getDefaultUncaughtExceptionHandler().uncaughtException(Thread.currentThread(), t);
+            }
+        }
+    }
+
+
+    /**
+     * Extract the exception (throwable) inside the ScheduledFutureTask
+     * @param runnable - The runable that was executed
+     * @return exception enclosed in the Runnable if any; null otherwise
+     */
+    private Throwable extractThrowable(Runnable runnable) {
+        // Check for exceptions wrapped by FutureTask.
+        // We do this by calling get(), which will cause it to throw any saved exception.
+        // Check for isDone to prevent blocking
+        if ((runnable instanceof Future<?>) && ((Future<?>) runnable).isDone()) {
+            try {
+                ((Future<?>) runnable).get();
+            } catch (CancellationException e) {
+                LOG.info("Task cancelled", e);
+            } catch (InterruptedException e) {
+                LOG.info("Task was interrupted", e);
+            } catch (ExecutionException e) {
+                return e.getCause();
+            }
+        }
+
+        return null;
+    }
+
+
+
 }
