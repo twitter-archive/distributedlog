9088f3236abedd7d83ad6223f239f1bf324c3d31
Leigh Stewart <lstewart@twitter.com>
2015-02-03 11:16:25 -0800

    dlog: bump birdcage sha to pickup finagle fixes - mostly we need the failfast/thriftmux fix from r/548254/ - took the jan 29 version
    RB_ID=564462

--- distributedlog-client/src/main/java/com/twitter/distributedlog/service/TestName.java
+++ distributedlog-client/src/main/java/com/twitter/distributedlog/service/TestName.java
@@ -9,6 +9,7 @@ import scala.runtime.BoxedUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.twitter.finagle.Addrs;
 import com.twitter.finagle.Addr;
 import com.twitter.finagle.Name;
 
@@ -23,7 +24,7 @@ public class TestName implements Name {
     public void changeAddrs(List<SocketAddress> addresses) {
         if (null != callback) {
             LOG.info("Sending a callback {}", addresses);
-            callback.apply(Addr.Bound$.MODULE$.apply(addresses));
+            callback.apply(Addrs.newBoundAddr(addresses));
         }
     }
 }
