8d5c430a5b3e7f9041b03d37fdf6164c79722dc6
Dave Rusek <dave.rusek@gmail.com>
2016-05-12 20:18:32 -0600

    Fixes #14: remove concurrency from ConfigurationSubscription tests (#16)

--- distributedlog-core/src/main/java/com/twitter/distributedlog/config/ConcurrentConstConfiguration.java
+++ distributedlog-core/src/main/java/com/twitter/distributedlog/config/ConcurrentConstConfiguration.java
@@ -18,18 +18,12 @@
 package com.twitter.distributedlog.config;
 
 import com.google.common.base.Preconditions;
-
 import org.apache.commons.configuration.Configuration;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Invariant thread-safe view of some configuration.
  */
 public class ConcurrentConstConfiguration extends ConcurrentBaseConfiguration {
-    static final Logger LOG = LoggerFactory.getLogger(ConcurrentConstConfiguration.class);
-
     public ConcurrentConstConfiguration(Configuration conf) {
         Preconditions.checkNotNull(conf);
         copy(conf);
--- distributedlog-core/src/test/java/com/twitter/distributedlog/config/TestConfigurationSubscription.java
+++ distributedlog-core/src/test/java/com/twitter/distributedlog/config/TestConfigurationSubscription.java
@@ -17,21 +17,20 @@
  */
 package com.twitter.distributedlog.config;
 
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-
 import com.google.common.collect.Lists;
 import com.twitter.distributedlog.DistributedLogConfiguration;
 import org.apache.commons.configuration.event.ConfigurationEvent;
 import org.apache.commons.configuration.event.ConfigurationListener;
+import org.jmock.lib.concurrent.DeterministicScheduler;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.*;
 
@@ -46,20 +45,17 @@ public class TestConfigurationSubscription {
     @Test(timeout = 60000)
     public void testReloadConfiguration() throws Exception {
         PropertiesWriter writer = new PropertiesWriter();
-        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
         FileConfigurationBuilder builder = new PropertiesConfigurationBuilder(writer.getFile().toURI().toURL());
         ConcurrentConstConfiguration conf = new ConcurrentConstConfiguration(new DistributedLogConfiguration());
+        DeterministicScheduler executorService = new DeterministicScheduler();
         List<FileConfigurationBuilder> fileConfigBuilders = Lists.newArrayList(builder);
         ConfigurationSubscription confSub =
                 new ConfigurationSubscription(conf, fileConfigBuilders, executorService, 100, TimeUnit.MILLISECONDS);
-        final CountDownLatch reloadLatch = new CountDownLatch(1);
-        final AtomicReference<ConcurrentBaseConfiguration> confHolder =
-                new AtomicReference<ConcurrentBaseConfiguration>();
+        final AtomicReference<ConcurrentBaseConfiguration> confHolder = new AtomicReference<>();
         confSub.registerListener(new com.twitter.distributedlog.config.ConfigurationListener() {
             @Override
             public void onReload(ConcurrentBaseConfiguration conf) {
                 confHolder.set(conf);
-                reloadLatch.countDown();
             }
         });
         assertEquals(null, conf.getProperty("prop1"));
@@ -70,7 +66,6 @@ public class TestConfigurationSubscription {
 
         // reload the config
         confSub.reload();
-        reloadLatch.await();
         assertNotNull(confHolder.get());
         assertTrue(conf == confHolder.get());
         assertEquals("1", conf.getProperty("prop1"));
@@ -79,34 +74,20 @@ public class TestConfigurationSubscription {
     @Test(timeout = 60000)
     public void testAddReloadBasicsConfig() throws Exception {
         PropertiesWriter writer = new PropertiesWriter();
-        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+        DeterministicScheduler mockScheduler = new DeterministicScheduler();
         FileConfigurationBuilder builder = new PropertiesConfigurationBuilder(writer.getFile().toURI().toURL());
         ConcurrentConstConfiguration conf = new ConcurrentConstConfiguration(new DistributedLogConfiguration());
         List<FileConfigurationBuilder> fileConfigBuilders = Lists.newArrayList(builder);
         ConfigurationSubscription confSub =
-                new ConfigurationSubscription(conf, fileConfigBuilders, executorService, 100, TimeUnit.MILLISECONDS);
+                new ConfigurationSubscription(conf, fileConfigBuilders, mockScheduler, 100, TimeUnit.MILLISECONDS);
         assertEquals(null, conf.getProperty("prop1"));
 
         // add
         writer.setProperty("prop1", "1");
         writer.save();
-        ConfigTestUtil.waitForConfig(conf, "prop1", "1");
+        mockScheduler.tick(100, TimeUnit.MILLISECONDS);
         assertEquals("1", conf.getProperty("prop1"));
-        Thread.sleep(1000);
 
-        // update
-        writer.setProperty("prop1", "2");
-        writer.save();
-        ConfigTestUtil.waitForConfig(conf, "prop1", "2");
-        assertEquals("2", conf.getProperty("prop1"));
-        Thread.sleep(1000);
-
-        // remove
-        writer.removeProperty("prop1");
-        writer.save();
-        ConfigTestUtil.waitForConfig(conf, "prop1", null);
-        assertEquals(null, conf.getProperty("prop1"));
-        executorService.shutdown();
     }
 
     @Test(timeout = 60000)
@@ -119,12 +100,12 @@ public class TestConfigurationSubscription {
         writer.setProperty("prop5", "true");
         writer.save();
 
-        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+        ScheduledExecutorService mockScheduler = new DeterministicScheduler();
         FileConfigurationBuilder builder = new PropertiesConfigurationBuilder(writer.getFile().toURI().toURL());
         ConcurrentConstConfiguration conf = new ConcurrentConstConfiguration(new DistributedLogConfiguration());
         List<FileConfigurationBuilder> fileConfigBuilders = Lists.newArrayList(builder);
         ConfigurationSubscription confSub =
-                new ConfigurationSubscription(conf, fileConfigBuilders, executorService, 100, TimeUnit.MILLISECONDS);
+                new ConfigurationSubscription(conf, fileConfigBuilders, mockScheduler, 100, TimeUnit.MILLISECONDS);
         assertEquals(1, conf.getInt("prop1"));
         assertEquals("abc", conf.getString("prop2"));
         assertEquals(123.0, conf.getFloat("prop3"), 0);
@@ -138,12 +119,12 @@ public class TestConfigurationSubscription {
         writer.setProperty("prop1", "1");
         writer.save();
 
-        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+        DeterministicScheduler mockScheduler = new DeterministicScheduler();
         FileConfigurationBuilder builder = new PropertiesConfigurationBuilder(writer.getFile().toURI().toURL());
         ConcurrentConstConfiguration conf = new ConcurrentConstConfiguration(new DistributedLogConfiguration());
         List<FileConfigurationBuilder> fileConfigBuilders = Lists.newArrayList(builder);
         ConfigurationSubscription confSub =
-                new ConfigurationSubscription(conf, fileConfigBuilders, executorService, 100, TimeUnit.MILLISECONDS);
+                new ConfigurationSubscription(conf, fileConfigBuilders, mockScheduler, 100, TimeUnit.MILLISECONDS);
 
         final AtomicInteger count = new AtomicInteger(1);
         conf.addConfigurationListener(new ConfigurationListener() {
@@ -163,14 +144,14 @@ public class TestConfigurationSubscription {
         while (count.get() == initial) {
             writer.setProperty("prop1", Integer.toString(i++));
             writer.save();
-            Thread.sleep(100);
+            mockScheduler.tick(100, TimeUnit.MILLISECONDS);
         }
 
         initial = count.get();
         while (count.get() == initial) {
             writer.setProperty("prop1", Integer.toString(i++));
             writer.save();
-            Thread.sleep(100);
+            mockScheduler.tick(100, TimeUnit.MILLISECONDS);
         }
     }
 }
